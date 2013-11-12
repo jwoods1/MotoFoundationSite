1420da8d826c4e28649cbeb31eec9a68ceed3f57
b87775 <jwoods1@fullsail.edu>
2013-11-11 20:37:25 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotoFoundationSite::Application.config.secret_key_base = '41d439a8fe1e6d8ee376bfb0228412cb2c5102329c2dadab2a35edc49903288c59f8b64bc5d39b14355a0470ca656ccf0964dfa54a281a71868a17425e47bafd'
